9838983b4975216a5843279d13d19b50b68cad2f
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-10-03 01:18:18 +0300

    [lab3-1-BeverageEnhance] add basics

---
+++ lab3/BeverageEnhance/IBeverage.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+#include <memory>
+
+
+// Интерфейс "напиток"
+class IBeverage
+{
+public:
+	virtual ~IBeverage() = default;
+
+	virtual std::string GetDescription() const = 0;
+	virtual double GetCost() const = 0;
+};
+
+typedef std::unique_ptr<IBeverage> IBeveragePtr;
