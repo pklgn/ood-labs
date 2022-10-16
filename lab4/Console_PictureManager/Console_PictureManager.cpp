44da86cd326f26d0fbe46cc0d2b53a290641cf30
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-10-17 00:46:32 +0300

    [lab4] add simple console app with examples for input and output files

---
+++ lab4/Console_PictureManager/Console_PictureManager.cpp
@@ -0,0 +1,27 @@
+// Console_PictureManager.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include "../PictureManager/Designer/IDesigner.h"
+#include "../PictureManager/Designer/Designer.h"
+#include "../PictureManager/Painter/Painter.h"
+#include "../PictureManager/Canvas/ICanvas.h"
+#include "../PictureManager/Canvas/Canvas.h"
+
+int main()
+{
+	std::cout << "Enter input file name: ";
+	std::string inputFileName;
+	std::cin >> inputFileName;
+	std::cout << "Enter output file name: ";
+	std::string outputFileName;
+	std::cin >> outputFileName;
+	std::ifstream inputFile(inputFileName);
+	Canvas canvas(800, 600, "shapes");
+	Painter painter;
+	Designer designer;
+	painter.DrawPicture(designer.CreateDraft(inputFile), canvas);
+
+	canvas.CaptureShapes(outputFileName);
+}
