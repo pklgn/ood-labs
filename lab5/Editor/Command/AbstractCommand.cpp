b46b90b12f100de841f3a6891b0d668ff971e5a3
Pavel Ermakov <petechka_petrov_201@mail.ru>
2022-10-31 10:54:16 +0300

    [lab5] add basic commands

---
+++ lab5/Editor/Command/AbstractCommand.cpp
@@ -0,0 +1,20 @@
+#include "../pch.h"
+#include "AbstractCommand.h"
+
+void AbstractCommand::Execute()
+{
+	if (!m_executed)
+	{
+		DoExecute();
+		m_executed = true;
+	}
+}
+
+void AbstractCommand::Unexecute()
+{
+	if (m_executed)
+	{
+		DoUnexecute();
+		m_executed = false;
+	}
+}
