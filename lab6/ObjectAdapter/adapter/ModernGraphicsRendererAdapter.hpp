a361c6daaf27c8e06f97624d759efe18d3095ed4
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-11-14 01:20:53 +0300

    [lab6-ObjectAdapter] add first task

---
+++ lab6/ObjectAdapter/adapter/ModernGraphicsRendererAdapter.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include "../graphics_lib/graphics_lib.hpp"
+#include "../modern_graphics_lib/modern_graphics_lib.hpp"
+
+const modern_graphics_lib::Point DEFAULT_POINT{ 0, 0 };
+
+class ModernGraphicsRendererAdapter : public graphics_lib::ICanvas
+{
+public:
+	ModernGraphicsRendererAdapter(modern_graphics_lib::ModernGraphicsRenderer& adaptee)
+		: m_renderer(adaptee)
+		, m_startPoint(DEFAULT_POINT.x, DEFAULT_POINT.y)
+	{
+		m_renderer.BeginDraw();
+	}
+
+	~ModernGraphicsRendererAdapter()
+	{
+		m_renderer.EndDraw();
+	}
+
+	void MoveTo(int x, int y) override
+	{
+		SetStartPoint(x, y);
+	}
+
+	void LineTo(int x, int y) override
+	{
+		m_renderer.DrawLine(m_startPoint, modern_graphics_lib::Point(x, y));
+	}
+
+private:
+	void SetStartPoint(int x, int y)
+	{
+		m_startPoint.x = x;
+		m_startPoint.y = y;
+	}
+
+	modern_graphics_lib::Point m_startPoint;
+	modern_graphics_lib::ModernGraphicsRenderer& m_renderer;
+};
---
+++ lab6/ObjectAdapter/modern_graphics_lib/modern_graphics_lib.hpp
@@ -0,0 +1,76 @@
+﻿#pragma once
+#include <ios>
+#include <format>
+#include <iostream>
+
+// Пространство имен современной графической библиотеки (недоступно для изменения)
+namespace modern_graphics_lib
+{
+class Point
+{
+public:
+	Point(int x, int y)
+		: x(x)
+		, y(y)
+	{
+	}
+
+	int x;
+	int y;
+};
+
+// Класс для современного рисования графики
+class ModernGraphicsRenderer
+{
+public:
+	ModernGraphicsRenderer(std::ostream& strm)
+		: m_out(strm)
+	{
+	}
+
+	~ModernGraphicsRenderer()
+	{
+		if (m_drawing) // Завершаем рисование, если оно было начато
+		{
+			EndDraw();
+		}
+	}
+
+	// Этот метод должен быть вызван в начале рисования
+	void BeginDraw()
+	{
+		if (m_drawing)
+		{
+			throw std::logic_error("Drawing has already begun");
+		}
+		m_out << "<draw>" << std::endl;
+		m_drawing = true;
+	}
+
+	// Выполняет рисование линии
+	void DrawLine(const Point& start, const Point& end)
+	{
+		if (!m_drawing)
+		{
+			throw std::logic_error("DrawLine is allowed between BeginDraw()/EndDraw() only");
+		}
+		m_out << std::format(R"(  <line fromX="{}" fromY="{}" toX="{}" toY="{}"/>)", start.x, start.y, end.x, end.y)
+			  << std::endl;
+	}
+
+	// Этот метод должен быть вызван в конце рисования
+	void EndDraw()
+	{
+		if (!m_drawing)
+		{
+			throw std::logic_error("Drawing has not been started");
+		}
+		m_out << "</draw>" << std::endl;
+		m_drawing = false;
+	}
+
+private:
+	std::ostream& m_out;
+	bool m_drawing = false;
+};
+} // namespace modern_graphics_lib
