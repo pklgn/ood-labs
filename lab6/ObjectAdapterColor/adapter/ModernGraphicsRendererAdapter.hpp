8b3b5272b837549f39239c7e3b1f7297520624bb
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2022-11-14 03:42:15 +0300

    [lab6-ObjectAdapterColor] add third task without tests

---
+++ lab6/ObjectAdapterColor/adapter/ModernGraphicsRendererAdapter.hpp
@@ -0,0 +1,54 @@
+﻿#pragma once
+#include "../graphics_lib/graphics_lib.hpp"
+#include "../modern_graphics_lib/modern_graphics_lib.hpp"
+
+const modern_graphics_lib::Point DEFAULT_POINT{ 0, 0 };
+const modern_graphics_lib::RGBAColor DEFAULT_COLOR{ 0, 0, 0, 1 };
+
+class ModernGraphicsRendererAdapter : public graphics_lib::ICanvas
+{
+public:
+	ModernGraphicsRendererAdapter(modern_graphics_lib::ModernGraphicsRenderer& adaptee)
+		: m_renderer(adaptee)
+		, m_startPoint(DEFAULT_POINT.x, DEFAULT_POINT.y)
+		, m_color(DEFAULT_COLOR.r, DEFAULT_COLOR.g, DEFAULT_COLOR.g, DEFAULT_COLOR.a)
+	{
+		m_renderer.BeginDraw();
+	}
+
+	~ModernGraphicsRendererAdapter()
+	{
+		if (m_renderer.IsDrawing())
+		{
+			m_renderer.EndDraw();
+		}
+	}
+
+	void SetColor(uint32_t rgbColor) override
+	{
+		m_color.r = float((rgbColor >> 16) & 0xFF) / 0xFF;
+		m_color.g = float((rgbColor >> 8) & 0xFF) / 0xFF;
+		m_color.b = float(rgbColor & 0xFF) / 0xFF; 
+	}
+
+	void MoveTo(int x, int y) override
+	{
+		SetStartPoint(x, y);
+	}
+
+	void LineTo(int x, int y) override
+	{
+		m_renderer.DrawLine(m_startPoint, modern_graphics_lib::Point(x, y), m_color);
+	}
+
+private:
+	void SetStartPoint(int x, int y)
+	{
+		m_startPoint.x = x;
+		m_startPoint.y = y;
+	}
+
+	modern_graphics_lib::Point m_startPoint;
+	modern_graphics_lib::ModernGraphicsRenderer& m_renderer;
+	modern_graphics_lib::RGBAColor m_color;
+};
---
+++ lab6/ObjectAdapterColor/app/app.hpp
@@ -0,0 +1,39 @@
+﻿#pragma once
+#include "../shape_drawing_lib/shape_drawing_lib.hpp"
+#include "../modern_graphics_lib/modern_graphics_lib.hpp"
+#include "../adapter/ModernGraphicsRendererAdapter.hpp"
+
+namespace app
+{
+void PaintPicture(shape_drawing_lib::CanvasPainter& painter)
+{
+	using namespace shape_drawing_lib;
+
+	Triangle triangle({ 10, 15 }, { 100, 200 }, { 150, 250 }, 0xABCDEF);
+	Rectangle rectangle({ 30, 40 }, 18, 24, 0xAABBCC);
+
+	// TODO: нарисовать прямоугольник и треугольник при помощи painter
+	painter.Draw(triangle);
+	painter.Draw(rectangle);
+}
+
+void PaintPictureOnCanvas()
+{
+	graphics_lib::Canvas simpleCanvas;
+	shape_drawing_lib::CanvasPainter painter(simpleCanvas);
+	PaintPicture(painter);
+}
+
+void PaintPictureOnModernGraphicsRenderer()
+{
+	modern_graphics_lib::ModernGraphicsRenderer renderer(std::cout);
+
+	// TODO: при помощи существующей функции PaintPicture() нарисовать
+	// картину на renderer
+	// Подсказка: используйте паттерн "Адаптер"
+	ModernGraphicsRendererAdapter rendererAdapter(renderer);
+	shape_drawing_lib::CanvasPainter painter(rendererAdapter);
+	PaintPicture(painter);
+}
+
+} // namespace app
