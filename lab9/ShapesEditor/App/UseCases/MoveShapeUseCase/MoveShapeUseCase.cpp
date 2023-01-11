d7b7e28ca2311d966fd621e1d92be470ad8828b2
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2023-01-11 21:25:45 +0300

    [lab9] add move use case

---
+++ lab9/ShapesEditor/App/UseCases/MoveShapeUseCase/MoveShapeUseCase.cpp
@@ -0,0 +1,25 @@
+﻿#include "../../../pch.h"
+#include "../Commands/ChangeFrameCommand/ChangeFrameCommand.h"
+#include "MoveShapeUseCase.h"
+
+void MoveShapeUseCase::Move(const Point& offset)
+{
+	for (auto&& shape : m_shapesToMove)
+	{
+		auto frame = shape->GetFrame();
+		frame.left += offset.x;
+		frame.top += offset.y;
+		shape->SetFrame(frame);
+	}
+}
+
+void MoveShapeUseCase::Commit()
+{
+	for (auto&& shape : m_shapesToMove)
+	{
+		auto domainShape = shape->GetShape();
+		auto frame = shape->GetFrame();
+		// FIXED: создать команду по перемещению
+		m_history->AddAndExecuteCommand(std::make_unique<ChangeFrameCommand>(frame, domainShape));
+	}
+}
---
+++ lab9/ShapesEditor/App/UseCases/MoveShapeUseCase/MoveShapeUseCase.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "../../../common/Point/Point.h"
+#include "../../History/IHistory.h"
+#include "../../ShapeAppModel/ShapeAppModel.h"
+
+class MoveShapeUseCase
+{
+	MoveShapeUseCase(const std::vector<std::shared_ptr<ShapeAppModel>>&, std::shared_ptr<IHistory>);
+
+	void Move(const Point& offset);
+	void Commit();
+
+private:
+	std::vector<std::shared_ptr<ShapeAppModel>> m_shapesToMove;
+	std::shared_ptr<IHistory> m_history;
+};
