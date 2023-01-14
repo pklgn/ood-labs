f731552f01cde910cf4814dc9d076807476c414a
Pavel Ermakov <73781985+pklgn@users.noreply.github.com>
2023-01-14 20:23:12 +0300

    [lab9] add stream operators

--- lab9/ShapesEditor/common/Point/Point.h
+++ lab9/ShapesEditor/common/Point/Point.h
@@ -4,4 +4,18 @@ struct Point
 {
 	double x;
 	double y;
+
+	friend std::istream& operator>>(std::istream& inputStream, Point& point)
+	{
+		inputStream >> point.x >> point.y;
+
+		return inputStream;
+	}
+
+	friend std::ostream& operator<<(std::ostream& outputStream, const Point& point)
+	{
+		outputStream << point.x << " " << point.y;
+
+		return outputStream;
+	}
 };
